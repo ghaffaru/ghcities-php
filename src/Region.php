90be47ee49192282e5acfad76fcdfe30216d0a7c
Ghaffaru <mudashiruagm@gmail.com>
2021-02-18 10:29:44 +0000

    get all regions test pass

--- src/Region.php
+++ src/Region.php
@@ -4,5 +4,17 @@ namespace Ghaffaru\GhCities;
 
 class Region
 {
+    public static function all()
+    {
+        $json = json_decode(file_get_contents(__DIR__ . '/cities.json'));
 
+        $allRegions = [];
+
+        foreach ($json as $key => $value)
+        {
+            array_push($allRegions, $key);
+        }
+
+        return $allRegions;
+    }
 }
